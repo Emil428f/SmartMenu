58336143b7a4b4b76c28a5f6b506d13242c40777
Emil Helge Knochenhauer Jürgensen <emil428f@edu.eal.dk>
2018-10-09 21:39:54 +0200

    SmartMenu med test

---
+++ SmartMenuApp/SmartMenuApp/Bindings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunctionLibrary;
+
+namespace SmartMenuLibrary
+{
+    public class Bindings
+    {
+        public string call(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return Functions.DoThat();
+
+                case 2:
+                    return Functions.DoThis();
+
+                case 3:
+                    return Functions.DoSomething(Console.ReadLine());
+
+                case 4:
+                    return Functions.GetTheAnswerToLifeTheUniverseAndEverything().ToString();
+            }
+            return "Du kan kun vælge tal der er vist i menuen";
+        }
+        public void exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ SmartMenuApp/SmartMenuTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SmartMenuTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmartMenuTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ea3c88c-5aa6-4e4d-bea8-ab8daae9d8a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
